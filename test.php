8e9c2a36f3aed682f3be2c402bbf4a6943ea6a0c
Vishant <vishant@webbrainstechnologies.in>
2025-12-19 10:15:02 +0530

    #120: fix Trello card ID mapping

--- test.php
+++ test.php
@@ -2,14 +2,13 @@
 // Enter your code here, enjoy!
 $array = array(
 	"1" => "PHP code tester Sandbox Online",
-	"emoji" => "😀 😃 😄 😁 😆",
 	5,
 	5 => 89009,
 	"Random number" => rand(100, 999),
 	"PHP Version" => phpversion()
 );
 
-echo 'changes Added chenges to reflact commit to check';
+echo 'changes Added chenges to reflact commit to check have made chages';
 foreach ($array as $key => $value) {
 	echo $key . "\t=>\t" . $value . "\n";
 }
